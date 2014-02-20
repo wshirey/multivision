a034edce9c458b3270713f403054d7ba8b3389c2
Walter Shirey <waltshirey@gmail.com>
2014-02-20 15:12:31 -0600

    add user mongoose model

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -8,4 +8,30 @@ module.exports = function(config) {
   db.once('open', function callback() {
     console.log('multivision db opened');
   });
-};
+}
+
+var userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  username: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.find({}).remove(function() {
+  User.create({
+    firstName: 'Wally',
+    lastName: 'Shirey',
+    username: 'wshirey'
+  });
+  User.create({
+    firstName: 'Joe',
+    lastName: 'Namath',
+    username: 'joe cool'
+  });
+  User.create({
+    firstName: 'Pelé',
+    lastName: 'Nascimiento',
+    username: 'pelé'
+  });
+});
